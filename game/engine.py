45435cef6df3fa2d88ebbe9c43a39f49a30e40b9
Gord <gord@bolli.ca>
2019-08-12 17:11:57 -0700

    added basic game map procedural generation

--- game/engine.py
+++ game/engine.py
@@ -14,6 +14,10 @@ def main():
     map_width = 80
     map_height = 45
 
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
     colors = {
         'dark_wall': libtcod.Color(0, 0, 100),
         'dark_ground': libtcod.Color(50, 50, 150)
@@ -38,6 +42,8 @@ def main():
 
     # initialize game_map
     game_map = GameMap(map_width, map_height)
+    game_map.make_map(max_rooms, room_min_size, room_max_size,
+                      map_width, map_height, player)
 
     # variables for key and mouse inputs
     key = libtcod.Key()
