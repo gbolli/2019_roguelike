bd8cf88418eb3e564f0fa0c21747da27039ec6d3
Gord <gord@bolli.ca>
2019-08-30 09:47:37 -0700

    added scaling enemies and items by dungeon level

--- game/map_objects/game_map.py
+++ game/map_objects/game_map.py
@@ -11,7 +11,7 @@ from map_objects.rectangle import Rect
 from render_functions import RenderOrder
 from item_functions import heal, cast_lightning, cast_fireball, cast_confuse
 from game_messages import Message
-from random_utils import random_choice_from_dict
+from random_utils import random_choice_from_dict, from_dungeon_level
 
 
 class GameMap:
@@ -28,8 +28,7 @@ class GameMap:
         return tiles
 
     def make_map(self, max_rooms, room_min_size, room_max_size, map_width,
-                 map_height, player, entities, max_monsters_per_room,
-                 max_items_per_room):
+                 map_height, player, entities):
         # Create rooms and passages
         rooms = []
         num_rooms = 0
@@ -81,8 +80,7 @@ class GameMap:
                         self.create_h_tunnel(prev_x, new_x, new_y)
 
                 # add entities to the map
-                self.place_entities(new_room, entities, max_monsters_per_room,
-                                    max_items_per_room)
+                self.place_entities(new_room, entities)
 
                 # append new room to list
                 rooms.append(new_room)
@@ -115,18 +113,27 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def place_entities(self, room, entities, max_monsters_per_room,
-                       max_items_per_room):
+    def place_entities(self, room, entities):
         # get a random number of monsters
-        number_of_monsters = randint(0, max_monsters_per_room)
-        number_of_items = randint(0, max_items_per_room)
+        number_of_monsters = from_dungeon_level([[2, 1], [3, 4], [5, 6]],
+                                                self.dungeon_level)
+        number_of_items = from_dungeon_level([[1, 1], [2, 4]],
+                                             self.dungeon_level)
 
-        monster_chances = {'orc': 80, 'troll': 20}
+        monster_chances = {
+            'orc':
+            80,
+            'troll':
+            from_dungeon_level([[15, 3], [30, 5], [60, 7]], self.dungeon_level)
+        }
         item_chances = {
-            'healing_potion': 70,
-            'lightning_scroll': 10,
-            'fireball_scroll': 10,
-            'confusion_scroll': 10
+            'healing_potion': 35,
+            'lightning_scroll': from_dungeon_level([[25, 4]],
+                                                   self.dungeon_level),
+            'fireball_scroll': from_dungeon_level([[25, 6]],
+                                                  self.dungeon_level),
+            'confusion_scroll': from_dungeon_level([[10, 2]],
+                                                   self.dungeon_level)
         }
 
         for i in range(number_of_monsters):
@@ -254,9 +261,7 @@ class GameMap:
         self.tiles = self.initialize_tiles()
         self.make_map(constants['max_rooms'], constants['room_min_size'],
                       constants['room_max_size'], constants['map_width'],
-                      constants['map_height'], player, entities,
-                      constants['max_monsters_per_room'],
-                      constants['max_items_per_room'])
+                      constants['map_height'], player, entities)
 
         player.fighter.heal(player.fighter.max_hp // 2)
 
